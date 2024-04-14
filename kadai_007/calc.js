6d19e55d723e57fb24ca7a5feea4f53a8d6f095d
KazuhiroMurai9703 <166223246+KazuhiroMurai9703@users.noreply.github.com>
2024-04-14 09:59:06 +0900

    JavaScriptの基礎を学ぼう/【課題】条件分岐を使用して倍数を表示しよう/初回コミット

---
+++ kadai_007/calc.js
@@ -0,0 +1,28 @@
+// 変数宣言
+let num ;
+
+// 変数に代入
+//num = 99 ;　// 3の倍数。
+//num = 100 ; // 5の倍数。
+//num = 105 ;  // 3と5の倍数。
+num = 98 ;　// 3の倍数でも5の倍数でもない。
+
+if( num % 3 == 0 )
+{
+ console.log('3の倍数です');
+}
+
+if( num % 5 == 0 )
+{
+ console.log('5の倍数です');
+}
+
+if( num % 3 == 0 && num % 5 == 0 )
+{
+ console.log('3と5の倍数です');
+}
+else if ( num % 3 != 0 && num % 5 != 0)
+{
+ console.log( num );
+}
+
